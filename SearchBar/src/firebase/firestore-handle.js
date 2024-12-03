f8ad370b634a70e28d245704d49fcd7a90404f31
Evan Huynh <duong.huynh1100@gmail.com>
2024-12-02 18:51:30 -0600

    Firebase functions
    Added a remove and get function for the firebase database

--- SearchBar/src/firebase/firestore-handle.js
+++ SearchBar/src/firebase/firestore-handle.js
@@ -1,7 +1,7 @@
 import { db } from "./firebase-config";
 import { doc, setDoc, deleteDoc, getDoc } from "firebase/firestore"; 
 
-export const AddLocation = async (id, name, phone, website) => {
+export const addLocation = async (id, name, phone, website) => {
     const docRef = doc(db, id, name);
     getDoc(docRef).then((doc) => {
             setDoc(docRef, {
@@ -10,6 +10,24 @@ export const AddLocation = async (id, name, phone, website) => {
                 website: website
             });
     }).catch((error) => {
-        console.log("Error getting document:", error);
+        console.log("Error adding document:", error);
     });
+}
+
+export const removeLocation = async (id, name) => {
+    const location =  await getDoc(doc(db, id, name))
+    if (location.exists()) {
+        await deleteDoc(doc(db, id, name))
+    } else {
+        console.log("This location is not here")
+    }
+}
+
+export const getLocation = async (id, name) => {
+    const location =  await getDoc(doc(db, id, name))
+    if (location.exists()) {
+        return location.data()
+    } else {
+        console.log("Location could not be found")
+    }
 }
